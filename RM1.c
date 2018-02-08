f655ba2fa01a638a1c6d270017acfbe0cc8b082d
madhanmanilalap <35652995+madhanmanilalap@users.noreply.github.com>
2018-02-08 19:51:51 +0530

    Create RM1.c

---
+++ RM1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int c;
+clrscr();
+printf("Enter The String:\n");
+scanf("%d",&c);
+if(c<='0' || c>='9')
+{
+printf("\nYes");
+}
+else
+{
+printf("\nNo");
+}
+getch();
+}
