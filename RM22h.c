eef5a3b6d49ed1409fa73ef50a41b43c066e3afa
madhanmanilalap <35652995+madhanmanilalap@users.noreply.github.com>
2018-02-22 16:19:31 +0530

    Create RM22h.c

---
+++ RM22h.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("Enter the value a:");
+scanf("%d",&a);
+if(a%7==0)
+{
+printf("Yes");
+}
+else
+printf("No");
+getch();
+}
