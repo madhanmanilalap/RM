af8b090c18f70112663035466865892ebf71311b
madhanmanilalap <35652995+madhanmanilalap@users.noreply.github.com>
2018-03-02 14:18:53 +0530

    Create RM1i.c

---
+++ RM1i.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,i,d=0;
+clrscr();
+printf("Enter the value a:");
+scanf("%d",&a);
+printf("Enter the value b:");
+scanf("%d",&b);
+c=a*b;
+for(i=0;i<=c;i++)
+{
+if(c==i*i)
+{
+d=1;
+}}
+if(d==1)
+{
+printf("Yes");
+}
+else
+printf("No");
+getch();
+}
+
