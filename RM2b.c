3ba46de9cfaeb001b65507d03b4042b29d486b00
madhanmanilalap <35652995+madhanmanilalap@users.noreply.github.com>
2018-03-02 14:34:53 +0530

    Create RM2b.c

---
+++ RM2b.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float a,b,c;
+clrscr();
+printf("Enter the Length:");
+scanf("%f",&a);
+printf("Enter the Bradth:");
+scanf("%f",&b);
+c=a*b;
+printf("%.5f",c);
+getch();
+}
